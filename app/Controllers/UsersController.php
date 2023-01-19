78f33bf760167edc75fdf6960cbc138c19da799e
TFolquin <shendmails@gmail.com>
2023-01-19 12:36:32 +0100

    Only right methods are now allowed

--- app/Controllers/UsersController.php
+++ app/Controllers/UsersController.php
@@ -6,11 +6,9 @@ use App\Models\Users;
 
 class UsersController {
     
-    private $utils;
     private $model;
 
     function __construct(){
-        $this->utils = new PDOUtils();
         $this->model = new Users();
     }
 
@@ -27,7 +25,7 @@ class UsersController {
         default:
             $this->getAllUsers();
             break;
-    }
+        }
     }
     
     
@@ -39,19 +37,23 @@ class UsersController {
      * @return JSON of every users
      */
     public function getAllUsers(){
-        $result_set = $this->model->getAllUsers();
-        $result = [];
-        foreach ($result_set as $set) {
-            array_push(
-                $result,
-                [
-                    "id" => $set["id"],
-                    "username" => $set["username"]
-                ]
-            );
-        }
-        if (!is_null($result)) {
-            echo(json_encode($result));
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $result_set = $this->model->getAllUsers();
+            $result = [];
+            foreach ($result_set as $set) {
+                array_push(
+                    $result,
+                    [
+                        "id" => $set["id"],
+                        "username" => $set["username"]
+                    ]
+                );
+            }
+            if (!is_null($result)) {
+                echo (json_encode($result));
+            }
+        }else {
+            header('HTTP/1.1 405', true, 405);
         }
     }
     
@@ -60,24 +62,24 @@ class UsersController {
      */
     public function addUser(){
         $data = json_decode(file_get_contents('php://input'), true);
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if (!is_null($data) && !empty($data)) {
 
-        if (!is_null($data) && !empty($data)) {
-
-            if (isset($data['username']) && !empty($data['username']) && isset($data['password']) && !empty($data['password'])) {
-                if (gettype($data['username']) == "string" && gettype($data['password']) == "string" ) {
-                    $this->model->addUser($data['username'], $data['password']);
-                }else {
-                    header('HTTP/1.1 400 Wrong type', true, 400);
+                if (isset($data['username']) && !empty($data['username']) && isset($data['password']) && !empty($data['password'])) {
+                    if (gettype($data['username']) == "string" && gettype($data['password']) == "string") {
+                        $this->model->addUser($data['username'], $data['password']);
+                    } else {
+                        header('HTTP/1.1 400 Wrong type', true, 400);
+                    }
+                } else {
+                    header('HTTP/1.1 400 Username and password are not set', true, 400);
                 }
-                
-            }else {
-                header('HTTP/1.1 400 Username and password are not set', true, 400);
+            } else {
+                header('HTTP/1.1 400 Sometihng went wrong trying to get the request body', true, 400);
             }
-            
         }else {
-            header('HTTP/1.1 400 Sometihng went wrong trying to get the request body', true, 400);
-        }
+            header('HTTP/1.1 405', true, 405);
+        }  
     }
-
 }
 ?>
